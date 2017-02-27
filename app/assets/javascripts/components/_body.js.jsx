fa5b8c292043935b2a193e5df67344fe50ee40a3
[01;31m[K"name"[m[K: "John Lin <[01;31m[K"email"[m[K: "johnlin1214@gmail.com>
2017-02-26 17:31:16 -0800

    added api endpoints and responder gem

---
+++ app/assets/javascripts/components/_body.js.jsx
@@ -0,0 +1,9 @@
+var Body = React.createClass({
+  render() {
+    return (
+      <div>
+        <p>Body</p>
+      </div>
+    )
+  }
+});
---
+++ app/assets/javascripts/components/_header.js.jsx
@@ -0,0 +1,9 @@
+var Header = React.createClass({
+  render() {
+    return (
+      <div>
+        <p>Header</p>
+      </div>
+    )
+  }
+});
